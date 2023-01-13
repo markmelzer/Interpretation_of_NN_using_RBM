6dac0ee6286b60511d9347862dedb8f6796c793a
markmelzer <mark@fam-melzer.net>
2023-01-13 14:01:14 +0100

    Change model parameters, compute accuracy based on closest decision

--- SingleLayerPerceptonHPAIV.jl
+++ SingleLayerPerceptonHPAIV.jl
@@ -51,31 +51,43 @@ function get_data(MSA, AA_dict, cross, cv=10)
     lower_test = (cross - 1) * x + 1
     upper_test = cross * x
 
-    test_data = [(data_encoded[i,:], onehotbatch(decision[i], 0:1)) for i in lower_test+1:upper_test]
+    test_data = [(data_encoded[i,:], decision[i]) for i in lower_test+1:upper_test]
 
     if lower_test == 1
-        train_data = [(data_encoded[i,:], onehotbatch(decision[i], 0:1)) for i in upper_test+1:length(decision)]
+        train_data = [(data_encoded[i,:], decision[i]) for i in upper_test+1:length(decision)]
     elseif upper_test == length(decision)
-        train_data = [(data_encoded[i,:], onehotbatch(decision[i], 0:1)) for i in 1:lower_test]
+        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test]
     else
-        train_data = [(data_encoded[i,:], onehotbatch(decision[i], 0:1)) for i in 1:lower_test]
-        train_data = vcat(train_data, [(data_encoded[i,:], onehotbatch(decision[i], 0:1)) for i in upper_test+1:length(decision)])
+        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test]
+        train_data = vcat(train_data, [(data_encoded[i,:], decision[i]) for i in upper_test+1:length(decision)])
     end
 
-
     return train_data, test_data
+
 end
 
 # function that makes Perceptron
 function create_perceptron(input_length)
-    model = Flux.Dense(input_length => 2)
-    model
+    #Chain(
+    #    Flux.Dense(input_length => 10, relu),
+    #    Flux.Dense(10=>2, x->σ.(x))
+    #)
+    Flux.Dense(input_length => 1, identity)
 end
 
-# split training and test set (10-CV or 5-CV)
+# function to assign value to classification
+function evaluate(y_hat)
+    if y_hat > 0
+        return 1
+    end
+    return 0
+    
+end
+
+
 
 # function that trains Perceptron on data
-const loss = Flux.mse
+const loss = Flux.logitbinarycrossentropy
 
 function loss_and_accuracy(data, model, device)
     acc = 0
@@ -84,15 +96,16 @@ function loss_and_accuracy(data, model, device)
     for (x, y) in data
         x, y = device(x), device(y)
         y_hat = model(x)
-        ls += loss(y_hat, y, agg=sum)
-        acc += sum(onecold(y_hat) .== onecold(y))
+        ls += loss(evaluate.(y_hat), y, agg=sum)
+        acc += sum(evaluate.(y_hat) .== y)
         num += 1
     end
     return ls/num, acc/num
 end
 
-function train_perceptron(file::String, cv=10)
+function train_perceptron(file::String, cv=10, epochs = 7, seed = 42)
     device = cpu
+    Random.seed!(seed)
 
     # get data
     AA_dict = AA_to_int()
@@ -102,28 +115,34 @@ function train_perceptron(file::String, cv=10)
     avg_loss = 0
 
     model = 0
-    
+    weights = zeros(249)
     for cross in 1:cv
         train_data, test_data = get_data(MSA, AA_dict, cross, cv)
         # construct model
         model = create_perceptron(249) |> device # 249: length of MSA
 
         # optimizer
-        η::Float64 = 3e-8
-        opt = Flux.setup(Adam(η), model)
+        η::Float64 = 0.1
+        opt = Flux.setup(Descent(η), model)
         
         # Training
         test_loss, test_acc = 0, 0
-        for (x, y) in train_data
-            x, y = device(x), device(y)
-            gs = gradient(m -> loss(m(x), onecold(y)), model)
-            Flux.Optimise.update!(opt, model, gs[1])
+        for epoch in 1:epochs
+            # shuffle!(train_data)
+            for (x, y) in train_data
+                #x, y = device(x), device(y)
+                #TODO: trains for 0 and 1, not positive or negative 
+                gs = gradient(m -> loss(m(x), y), model)
+                Flux.Optimise.update!(opt, model, gs[1])
+            end
         end
 
+
+        weights .+= abs.(model.weight[1,:]/cv)
         # report on train and test
-        train_loss, train_acc = loss_and_accuracy(train_data, model, device)
+        #train_loss, train_acc = loss_and_accuracy(train_data, model, device)
         test_loss, test_acc = loss_and_accuracy(test_data, model, device)
-        println(" train_loss = $train_loss, train_accuracy = $train_acc")
+        #println(" train_loss = $train_loss, train_accuracy = $train_acc")
         println(" test_loss = $test_loss, test_accuracy = $test_acc")
         
         avg_acc += test_acc/cv
@@ -132,7 +151,7 @@ function train_perceptron(file::String, cv=10)
     println("Avg. accuracy: ", avg_acc, "\t avg. loss: $avg_loss")
     
     # return last model
-    model
+    return model, weights
 end
 
 # function that evaluates performance
