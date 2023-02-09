9da2148503d6ad5b8364522efa4468c8054a7012
markmelzer <mark@fam-melzer.net>
2023-02-09 15:06:20 +0100

    Functions to compute the value of the neurons in the network for a MSA

--- src/EvaluateNetwork.jl
+++ src/EvaluateNetwork.jl
@@ -1,28 +1,42 @@
+using DelimitedFiles
 include("TrainNetwork.jl")
+include("DataPreparation.jl")
 
 
 params = Hyperparameters()
 data = DataParameters(file_name = "./data/HPAIV_train_set.csv", input_length = 249, aa_universe = "ABCDEFGHIKLMNPQRSTVWY?")
 
-model, avg_acc, avg_loss, encoded_data, data = train_network(params, data)
+#model, avg_acc, avg_loss, AA_dict = train_network(params, data)
 
-l = 1
+val = evaluateTestData(model, "./data/HPAIV_test_set.csv", AA_dict, "./results/output_node_values.csv")
 
-layer = model.layers[l]
-weights = layer.weight
-bias = layer.bias
-
-# TODO: this is only for linear activation function in this layer!!! (can check it out with layer.σ)
-values_in_layer = (encoded_data[:,1:end-1] * weights')' .+ bias
-
-
-# write to files: encoded amino acid sequence + decision, values in layers
-using DelimitedFiles
-writedlm("UsedMSANS1.csv", data, ',')
-writedlm("NodeValues2L3N.csv", values_in_layer, ',')
+function evaluateLayer(model, layer, input_data)
+    # assume encoded input data, without the decision (only containing features)
+    value = (input_data' * model.layers[layer].weight')' .+ model.layers[layer].bias
+    model.layers[layer].σ(value)
+end
 
 
-# return model
+function evaluateNetwork(model, input_data)
+    # return node values for one input
+    # assume encoded input data, without the decision (only containing features)
+    model_values = [input_data]
+    for lay in 1:length(model) 
+        #println(model_values, '\t', evaluateLayer(model, lay, input_data))
+        append!(model_values, [evaluateLayer(model, lay, input_data)])
+    end
+    model_values
+end
 
-# for each layer 
-    # compute values of nodes, based on input (or values of previous network), weights, bias and activation function
+
+# function to encode data, and run evaluateNetwork for all data points
+function evaluateTestData(model, data_file, AA_dict, output_name)
+    MSA = readdlm(data_file, ',')
+    encoded_MSA = MSA_encode(MSA, AA_dict)
+    model_values = [["Output for model with the following amount of layers: ", length(model)]]
+    for i in 1:size(encoded_MSA)[1]
+        append!(model_values, evaluateNetwork(model, encoded_MSA[i,:]))
+    end
+    writedlm(output_name, model_values, ',')
+    model_values
+end
