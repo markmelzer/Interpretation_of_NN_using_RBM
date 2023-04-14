6bb1c97b6540dc3567d4c8f956c1268e4d837ba8
markmelzer <mark@fam-melzer.net>
2023-04-14 10:29:55 +0200

    Transcriptome Processing: add safety by using experiment summary, follow standard edgeR normalization

--- src/TranscriptomeProcessing.R
+++ src/TranscriptomeProcessing.R
@@ -5,16 +5,16 @@ getwd()
 set.seed(0)
 
 # get data
-load("C:/Users/Mark/Downloads/E-GEOD-68086-atlasExperimentSummary.Rdata")
+load("../data/E-GEOD-68086-atlasExperimentSummary.Rdata")
 
-#raw <- read.csv("../data/GeneExpression/E-GEOD-68086-raw-counts.tsv", sep = '\t')
 raw <- experiment_summary@listData$rnaseq@assays$data@listData$counts
+batches <- experiment_summary@listData$rnaseq@colData$block
 
 dim(raw)
 # 65217 genes
 
-# as in paper: exclude gnes with less than five (non-normalized) read counts in all samples
-raw <- raw[-which(apply(raw, 1, sum) <= 5),]
+# as in paper: exclude genes with less than five (non-normalized) read counts in all samples
+raw <- raw[-which(apply(raw, 1, sum) < 5),]
 # - 8419 genes
 
 dim(raw)
@@ -22,92 +22,83 @@ dim(raw)
 
 
 # only use patients of breast cancer study
-breast = raw[,c(3:26,252:266)]
-reference = raw[,c(115:170, 249:251)]
-reference = reference[,-32]
-dim(breast)
-dim(reference)
+dis <- experiment_summary@listData$rnaseq@colData$disease
+
+subjects <- which(dis %in% c("normal", "breast carcinoma"))
+#subjects <- which(dis %in% c("normal", "non-small cell lung carcinoma"))
+
+label <- dis[subjects]
+label[which(label != "normal")] = 1
+label[which(label == "normal")] = 0
+label = as.numeric(label)
+data <- raw[,subjects]
+
+# get batches 
+batch <- batches[subjects]
 
 
-data <- data.frame(cbind(breast, reference))
-#data <- raw
 
-
+library(edgeR)
 # weighted trimmed mean of M-values (TMM) --> library norm factor
 y <- calcNormFactors(data)
 
-# labels: 1 for breast cancer, 0 for reference
-label <- append(rep(1, dim(breast)[2]), rep(0, dim(reference)[2]))
+
+# create design matrix
+design <- model.matrix(~ batch + label)
 
 # create DGE list from data
 dge <- DGEList(counts = data, group = label, norm.factors = y)
 
-
 # compute dispersion
-disp <- estimateDisp(dge)
+disp <- estimateDisp(dge, design)
+plotBCV(disp)
+
+# =============================================================================
+# fit generalized linear model
+g <- glmFit(disp, design)
+gt <- glmLRT(g, coef = length(colnames(design)))
+t <- gt$table
+
+# make color for plot
+col = unname(abs(decideTests(gt)))
+col[col == 0,] = "black"
+col[col == 1,] = "red"
+
+plot(2^t$logCPM, t$logFC, log = "x", col = col)
 
 
-
+# =============================================================================
 # fit (negative binomial) generalized linear model
-glm <- glmQLFit(disp)
 
-test <- glmQLFTest(glm)
+glm <- glmQLFit(disp, design)
+plotQLDisp(glm)
 
+test <- glmQLFTest(glm, coef = 6)
 
-tt <- topTags(test, n=Inf)$table
-tt$decision <- unname(abs(decideTests(test)))
+tt <- test$table
+#tt$decision <- unname(abs(decideTests(test)))
+col = unname(abs(decideTests(test)))
+col[col == 0,] = "black"
+col[col == 1,] = "red"
 
-plot(2^tt$logCPM, tt$logFC, log = "x", col = unname(abs(decideTests(test))))
-
-plot(2^tt$logCPM, tt$logFC, log = "x")
-
-
-#================= test ===================================
-
-high_exp_genes <- data.frame(test$fitted.values[rownames(tt[tt$logCPM >= 3,]),])
+plot(2^(tt$logCPM), tt$logFC, log = "x", col = col)
 
 
-#tmp = t(rbind(high_exp_genes, label))
-#write.csv(tmp, "high_exp_genes.csv")
-
-# clustering: Pearson distances, Ward clustering
-
-library(ClassDiscovery)
-d <- distanceMatrix(high_exp_genes, "pearson")
-
-cl <- hclust(d, method = "ward.D")
-
-
-b = data.frame(test$fitted.values[,1:39])
-c = data.frame(test$fitted.values[,40:97])
-
-b_means = apply(b, 1, mean)
-c_means = apply(c, 1, mean)
-
-b_means[b_means == 0] = 10^-8
-c_means[c_means == 0] = 10^-8
-
-
-M = log2(b_means/c_means)
-A = 1/2 * (b_means+c_means)
-
-
-plot(A, M, log = "x", ylim = c(-10, 10), xlim = c(0.001, 10^7))
-
-df = data.frame(M, 2^A)
-
-ggplot(df, aes(x=A,y=M)) + 
-  geom_point() +
-  scale_x_log10() +
-  ylim(-10, 10)
+is.de <- decideTests(test, p.value=0.05)
+summary(is.de)
 
 
 
+# compare with EBI results
+truth = read.csv("C:/Users/Mark/Downloads/E-GEOD-68086-analytics.tsv", sep = '\t', header = T)
+FC = truth$X.breast.carcinoma..vs..normal..log2foldchange
+res <- tt$logFC
+
+qqplot(FC, res)
+plot(2^(tt$logCPM), FC, log = "x", col = col)
 
 
-
-
-
-
-
-
+# save fitted values to csv and RDS
+write.table(test$fitted.values, file = "../data/normalized_GE_data.csv",
+            quote = F, sep = ',', row.names = T, col.names = T)
+saveRDS(test$fitted.values, "../data/normalized_GE_data.RDS")
