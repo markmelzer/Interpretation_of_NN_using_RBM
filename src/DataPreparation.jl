9cff520421612a0d0d6353336ad7973a1676f2c2
markmelzer <mark@fam-melzer.net>
2023-04-14 10:18:22 +0200

    Fix code for CV, so no object is left out

--- src/DataPreparation.jl
+++ src/DataPreparation.jl
@@ -4,6 +4,7 @@
 =#
 
 using Parameters
+using Random
 
 # creating dictionary AA to integers
 function AA_to_int(data)
@@ -47,6 +48,8 @@ function undersample(data)
     # get number of objects belonging to smaller decision class
     count = sum(data[:,end])
     len = length(data[:,end])
+    println(count, '\t', len)
+    println(data[:,end])
     if count > len-count 
         smaller = 0
         bigger = 1
@@ -58,34 +61,48 @@ function undersample(data)
     end
     # choose same number of bigger decision class randomly
     tmp = data[findall(data[:,end].==bigger),:]
-    pos = rand(1:size(tmp)[1], num)
+    pos = rand(1:size(tmp)[1], Int(num))
 
     # return balanced data matrix
     vcat(tmp[pos,:], data[findall(data[:,end].==smaller),:])
 end
 
 
-# get data 
-function get_data(MSA, AA_dict, cross, cv=10)
+# get data for amino acid MSA
+function get_data_AA(MSA, AA_dict, cross, cv=10)
     # MSA object should not have a header or rownames !
     data_encoded = MSA_encode(MSA, AA_dict)
-    decision = MSA[1:end, end]
+    # data_encoded = undersample(MSA)
+    decision = MSA[:, end]
 
-    x = trunc(Int, length(decision)/cv)
-    lower_test = (cross - 1) * x + 1
-    upper_test = cross * x
 
-    test_data = [(data_encoded[i,:], decision[i]) for i in lower_test+1:upper_test]
+    upper_test = min(trunc(Int, cross * round(length(decision)/cv)), size(data_encoded)[1])
+    lower_test = (cross - 1) * trunc(Int, round(length(decision)/cv)) + 1
+
+    test_data = [(data_encoded[i,:], decision[i]) for i in lower_test:upper_test-1]
 
     if lower_test == 1
-        train_data = [(data_encoded[i,:], decision[i]) for i in upper_test+1:length(decision)]
+        train_data = [(data_encoded[i,:], decision[i]) for i in upper_test:length(decision)]
     elseif upper_test == length(decision)
-        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test]
+        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test-1]
+        test_data = vcat(test_data, [(data_encoded[end,:], decision[end])])
     else
-        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test]
-        train_data = vcat(train_data, [(data_encoded[i,:], decision[i]) for i in upper_test+1:length(decision)])
+        train_data = [(data_encoded[i,:], decision[i]) for i in 1:lower_test-1]
+        train_data = vcat(train_data, [(data_encoded[i,:], decision[i]) for i in upper_test:length(decision)])
     end
-
+    
     return train_data, test_data
+
+end
+
+
+# get data for continuous data
+function get_data(MSA)
+    # data object should not have a header or rownames, but the decision !
+    
+    features = MSA[:,1:end-1]
+    decision = MSA[1:end, end]
+
+    return [(features[i,:], decision[i]) for i in 1:length(decision)]
 
 end
