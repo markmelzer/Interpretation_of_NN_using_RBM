c2d14d95e28b849911d8db0602c67f2508347a2d
markmelzer <mark@fam-melzer.net>
2023-04-14 10:19:57 +0200

    Add code to save trained NN, update function calls to differentiate between discrete (AA) and continuous data

--- src/EvaluateNetwork.jl
+++ src/EvaluateNetwork.jl
@@ -1,14 +1,19 @@
 using DelimitedFiles
-include("TrainNetwork.jl")
-include("DataPreparation.jl")
+#include("TrainNetwork.jl")
+#include("DataPreparation.jl")
 
 
-params = Hyperparameters()
-data = DataParameters(file_name = "./data/HPAIV_train_set.csv", input_length = 249, aa_universe = "ABCDEFGHIKLMNPQRSTVWY?")
+params = Hyperparameters(mode = "single")
+data = DataParameters(file_name = "./data/HPAIV_train_set_imp_feat.csv", input_length = 112, aa_universe = "ABCDEFGHIKLMNPQRSTVWY?")
 
-#model, avg_acc, avg_loss, AA_dict = train_network(params, data)
+#model, avg_acc, avg_loss, AA_dict = train_network_AA(params, data)
 
-val = evaluateTestData(model, "./data/HPAIV_test_set.csv", AA_dict, "./results/output_node_values.csv")
+m, acc, loss, dict, l, w = train_network_AA(params, data)
+val = evaluateTestData(m, "./data/HPAIV_test_set_imp_feat.csv", dict, "./results/output_node_values.csv")
+
+w = m.weight
+x = sortperm(abs.(w'[:,1]))
+# some kind of cutoff
 
 function evaluateLayer(model, layer, input_data)
     # assume encoded input data, without the decision (only containing features)
@@ -33,10 +38,20 @@ end
 function evaluateTestData(model, data_file, AA_dict, output_name)
     MSA = readdlm(data_file, ',')
     encoded_MSA = MSA_encode(MSA, AA_dict)
-    model_values = [["Output for model with the following amount of layers: ", length(model)]]
+
+    measures = [0, 0, 0, 0]
     for i in 1:size(encoded_MSA)[1]
-        append!(model_values, evaluateNetwork(model, encoded_MSA[i,:]))
+        pred = model(encoded_MSA[i,:])
+        measures += performance_measure(MSA[i, end], evaluate(pred[1]))
     end
-    writedlm(output_name, model_values, ',')
-    model_values
+    
+    measures
 end
+
+# save model
+#using BSON: @save
+#@save "./results/Exp1_HPAIV/vanilla_model.bson" m
+
+
+#using BSON: @load
+#@load "./results/Exp1_HPAIV/vanilla_model.bson" m
