bbf688af65274ab3d506572b52abf83b39969a46
markmelzer <mark@fam-melzer.net>
2023-04-14 10:26:11 +0200

    Add function to plot loss over epoch

--- src/Plots.jl
+++ src/Plots.jl
@@ -49,3 +49,28 @@ function heatmap_comparison(rosetta, julia, structure)
     ylabel!("Rosetta          Perceptron        Structure")
     title!("Comparison of found significant Features")
 end
+
+function plot_losses(losses)
+    #=
+    Function to plot the loss per epoch.
+    =#
+    mini = minimum(losses[1])
+    maxi = maximum(losses[1])
+    x = range(1, length(losses[1]), length(losses[1]))
+
+    plot(x, losses[1])
+    if length(losses) > 1
+        for loss in losses[2:end]
+            plot!(x, loss)
+            # mini = minimum([minimum(loss), mini])
+            # maxi = maximum([maximum(loss), maxi])
+        end
+    end
+
+    xlabel!("Epoch")
+    xlims!(0, length(loss[1]))
+    ylabel!("Loss")
+    #ylims!(mini - 0.1, maxi + 0.1)
+    #rosetta = [84, 91, 47, 49, 107, 109, 180, 182, 96, 98]
+    #vline!(rosetta)
+end
