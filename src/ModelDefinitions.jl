15ee5d5e7da67b2e72bdf717ba268d284ecf38aa
markmelzer <mark@fam-melzer.net>
2023-04-14 10:25:06 +0200

    Use same activation function in each layer

--- src/ModelDefinitions.jl
+++ src/ModelDefinitions.jl
@@ -11,15 +11,15 @@ function create_perceptron(params, data)
     #    Flux.Dense(10=>2, x->σ.(x))
     #)
     if mode == "chain"
-        return Flux.Chain(Flux.Dense(input_length => input_length, Flux.celu),
+        return Flux.Chain(Flux.Dense(input_length => input_length, actFunction),
         Flux.Dense(input_length => input_length, actFunction),
-        Flux.Dense(input_length => input_length, Flux.relu),
-        Flux.Dense(input_length => input_length, Flux.σ),
-        Flux.Dense(input_length => input_length, Flux.celu),
         Flux.Dense(input_length => input_length, actFunction),
-        Flux.Dense(input_length => input_length, Flux.relu),
-        Flux.Dense(input_length => input_length, Flux.σ),
-        Flux.Dense(input_length => input_length, Flux.celu),
+        Flux.Dense(input_length => input_length, actFunction),
+        Flux.Dense(input_length => input_length, actFunction),
+        Flux.Dense(input_length => input_length, actFunction),
+        Flux.Dense(input_length => input_length, actFunction),
+        Flux.Dense(input_length => input_length, actFunction),
+        Flux.Dense(input_length => input_length, actFunction),
         Flux.Dense(input_length => 1, actFunction))
     end
     Flux.Dense(input_length => 1, actFunction)
