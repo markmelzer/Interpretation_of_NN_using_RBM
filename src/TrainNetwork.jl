067bd3d0b290068e302382ad4807ea82d21a74ec
markmelzer <mark@fam-melzer.net>
2023-06-29 10:38:42 +0200

    Update function training the NN

--- src/TrainNetwork.jl
+++ src/TrainNetwork.jl
@@ -4,21 +4,14 @@ using Flux.Data: DataLoader
 using Flux: onehotbatch, onecold, onehot, @epochs
 using NNlib
 using DelimitedFiles
-include("DataPreparation.jl")
-include("Evaluation.jl")
-include("ModelDefinitions.jl")
-include("Parameters.jl")
-
-
-# call parameters
-#params = Hyperparameters(mode = "single", optimizer = Flux.Adam)
-#data = DataParameters(file_name = "./data/HPAIV_train_set.csv", input_length = 249, aa_universe = "ABCDEFGHIKLMNPQRSTVWY?")
-
+using Parameters
 
 function training(MSA, AA_dict, params, data, device)
+    # train network on the full input data
     @unpack lossFunction, η, optimizer, epochs = params
     @unpack file_name, input_length = data
     
+    # encode data
     MSA = MSA[shuffle(1:end), :]
     data_encoded = MSA_encode(MSA, AA_dict)
     decision = MSA[1:end, end]
@@ -34,6 +27,7 @@ function training(MSA, AA_dict, params, data, device)
     for epoch in 1:epochs
         for (x, y) in train
             # x, y = device(x), device(y)
+            y = onehotbatch(y, 0:1)
             gs = gradient(m -> lossFunction(m(x), y), model)
             Flux.Optimise.update!(opt, model, gs[1])
         end
@@ -42,23 +36,18 @@ function training(MSA, AA_dict, params, data, device)
 end
 
 
-# this function is to train the NN on discrete MSA data
+# this function is to train the NN on discrete MSA data with CV
 function train_network_AA(params, data, AminoAcidEncoding)
+    # train network while performing CV, then on full data
     device = cpu
     @unpack lossFunction, η, optimizer, epochs, cv, seed, mode = params
     @unpack file_name, input_length = data
 
-
-
     Random.seed!(seed)
-
+    
     # get data
     @unpack AA_dict = AminoAcidEncoding
     MSA = readdlm(file_name, ',')
-    
-    # undersampling might be necessary
-    #MSA = undersample(MSA[1:end,:])
-
     #shuffle MSA (very important, otherwise very bad results)
     MSA = MSA[shuffle(1:end), :]
 
@@ -69,10 +58,6 @@ function train_network_AA(params, data, AminoAcidEncoding)
     loss = []
     weights = zeros(input_length, 2)
 
-    #model = 0
-
-    # TODO: make functions for epoch, optimizing part etc
-
     for cross in 1:cv
         train_data, test_data = get_data_AA(MSA, AA_dict, cross, cv)
         # construct model
@@ -80,29 +65,20 @@ function train_network_AA(params, data, AminoAcidEncoding)
 
         # optimizer
         opt = Flux.setup(optimizer(η), model)
-
-        # append array to loss to save loss in each epoch
-        #append!(loss, [[loss_and_accuracy(test_data, model, device, params)[1]]])
-
-        # append array to weights to save weights in each epoch
-        #append!(weights, [[model.layers[1].weight[:,1]]])
         
         # Training
         test_loss, test_acc = 0, 0
         for epoch in 1:epochs
             for (x, y) in train_data
-                #x, y = device(x), device(y)
+                x, y = device(x), device(y)
                 y = onehotbatch(y, 0:1)
                 gs = gradient(m -> lossFunction(m(x), y), model)
                 Flux.Optimise.update!(opt, model, gs[1])
             end
-            #append!(loss[cross], loss_and_accuracy(test_data, model, device, params)[1])
-            #append!(weights[cross], [model.layers[1].weight[:,1]])
         end
 
         # evaluate performance for current fold
         test_loss, test_acc, perf_measure = loss_and_accuracy(test_data, model, device, params)
-        #weights += abs.(model.weight)'
         println(" test_loss = $test_loss, test_accuracy = $test_acc, Performance: $perf_measure")
         
         # update average performance
@@ -116,79 +92,6 @@ function train_network_AA(params, data, AminoAcidEncoding)
     # model trained on complete data
     model = training(MSA, AA_dict, params, data, device)
 
-    # return last model
+    # return model on complete data and further information
     return model, avg_acc, avg_loss, AA_dict, loss, weights
 end
-
-
-
-# this function is to train the NN on continuous data (e.g., gene expression)
-function train_network(params, data)
-    device = cpu
-    @unpack lossFunction, η, optimizer, epochs, cv, seed, mode = params
-    @unpack file_name, input_length = data
-
-    Random.seed!(seed)
-
-    # get data
-    dataset = readdlm(file_name, ',')
-    
-
-    #shuffle data SA (very important, otherwise very bad results)
-    dataset = dataset[shuffle(1:end), :]
-
-    # initialize performance measures
-    avg_acc = 0
-    avg_loss = 0
-    perf = [0, 0, 0, 0]
-
-
-    model = 0
-
-    # get data in right format
-    data_pairs = get_data(dataset)
-
-    # get indexes to cut for cv
-    idx = [trunc(Int, round(i*length(data_pairs)/cv)) for i in 0:cv]
-
-    # TODO: what if cv = 1
-    for cross in 1:cv
-        # get test and train data for current cv fold
-        test_data = data_pairs[idx[cross]+1:idx[cross+1]]
-        train_data = data_pairs[filter(x->!(x in vcat(idx[cross]+1:idx[cross]...)), eachindex(data_pairs))]
-
-        # construct model
-        model = create_perceptron(params, data) |> device
-
-        # optimizer
-        opt = Flux.setup(optimizer(η), model)
-        
-        # Training
-        test_loss, test_acc = 0, 0
-        for epoch in 1:epochs
-            for (x, y) in train_data
-                #x, y = device(x), device(y)
-                gs = gradient(m -> lossFunction(m(x), y), model)
-                Flux.Optimise.update!(opt, model, gs[1])
-            end
-        end
-
-        # evaluate performance for current fold
-        test_loss, test_acc, perf_measure = loss_and_accuracy(test_data, model, device, params)
-        println(" test_loss = $test_loss, test_accuracy = $test_acc, Performance: $perf_measure")
-        
-        # update average performance
-        avg_acc += test_acc/cv
-        avg_loss += test_loss/cv
-        perf += perf_measure
-    end
-
-    println("Avg. accuracy: ", avg_acc, "\t avg. loss: $avg_loss", "\t Performance: $perf")
-    
-    # model trained on complete data
-    #model = training(MSA, AA_dict, params, data, device)
-
-    # return last model
-    return model, avg_acc, avg_loss
-end
-
